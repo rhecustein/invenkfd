cb5a6975f5a216e97ed6f954d16485ab1cd92dd4
Faiz <faizadli9912@gmail.com>
2022-01-14 10:35:56 +0700

    inventori update

--- resources/views/inventoris.blade.php
+++ resources/views/inventoris.blade.php
@@ -1,5 +1,10 @@
 @extends('layouts.kfd')
 @section('content')
-inventiri
+<div class="content">
+    <div class="main">
+        <button class="btn btn-primary me-2">Primary</button>
 
+
+    </div>
+</div>
 @endsection
