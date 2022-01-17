63d6ec9def33450562781ed1a177c8ad36988159
Faiz <faizadli9912@gmail.com>
2022-01-17 13:22:53 +0700

    chart dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -58,10 +58,48 @@
                 </div>
             </div>
         </div>
-        <div class="card">
-            <div class="card-body">
-                <h4>Lorem Ipsum</h4>
-                <div id="dashline-chart"></div>
+        <div class="row">
+            <div class="col-md-7">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="mb-3">Lorem Ipsum</h4>
+                        <div class="row">
+                            <div class="col-6 col-md-3">
+                                <div class="text-center py-3 border-end">
+                                    <h5>3215</h5>
+                                    <span class="text-muted">Lorem Ipsum</span>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <div class="text-center py-3 border-end">
+                                    <h5>124731</h5>
+                                    <span class="text-muted">Lorem Ipsum</span>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <div class="text-center py-3 border-end">
+                                    <h5>3238947</h5>
+                                    <span class="text-muted">Lorem Ipsum</span>
+                                </div>
+                            </div>
+                            <div class="col-6 col-md-3">
+                                <div class="text-center py-3">
+                                    <h5>7123</h5>
+                                    <span class="text-muted">Lorem Ipsum</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div id="overview-chart"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="mb-0">Recent Transaction</h4>
+                        <div id="basic-bar-chart"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
