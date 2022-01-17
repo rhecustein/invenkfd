ecaa7e9a9a55dfaaa5d28943b2b77b8cc00bd4a3
ahdadzia <ahda.fakhri@gmail.com>
2022-01-17 11:27:44 +0700

    Routing tombol simpan barang kembali ke inventoris & mencoba membuat datepicker form

--- resources/views/inven/create.blade.php
+++ resources/views/inven/create.blade.php
@@ -2,13 +2,52 @@
 @section('content')
 
 
-<div class="mb-3">
+{{-- <div class="mb-3">
     <label for="formGroupExampleInput" class="form-label">Example label</label>
     <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input placeholder">
 </div>
 <div class="mb-3">
     <label for="formGroupExampleInput2" class="form-label">Another label</label>
     <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder">
+</div> --}}
+
+<div class="mb-3">
+    <label>Nama Barang</label>
+    <input type="text" class="form-control" name="judul" id="formGroupExampleInput">
+</div>
+
+<div class="mb-3">
+    <label>Kategori</label>
+    <select class="form-control" name="category_id">
+        <option holder>Pilih Kategori</option>
+    </select>
+</div>
+
+<div class="mb-3">
+    <label for="">Tags</label>
+    <select class="form-control" name="tags[]">
+        <option holder>Pilih Tag</option>
+    </select>
+</div>
+
+<div class="mb-3">
+    <label>Keterangan Barang</label>
+    <textarea class="form-control" name="content"></textarea>
+</div>
+
+{{-- <div class="container mt-5" style="max-width: 450px">
+    <div class="form-group">
+        <div class='input-group date' id='datetimepicker'>
+        <input type='text' class="form-control" />
+        <span class="input-group-addon">
+          <span class="glyphicon glyphicon-calendar"></span>
+        </span>
+        </div>
+    </div>
+</div> --}}
+
+<div class="mb-3">
+    <a href="{{ route('inventoris') }}"><button class="btn btn-primary btn-block">Simpan Barang</button></a>
 </div>
 
 @endsection
--- resources/views/layouts/kfd.blade.php
+++ resources/views/layouts/kfd.blade.php
@@ -14,6 +14,10 @@
     <!-- Core css -->
     <link href="{{ asset('html/demo/app/assets/css/app.min.css') }}" rel="stylesheet">
 
+    <!-- Datetime Picker CSS -->
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css"> --}}
+
 </head>
 
 <body>
@@ -522,6 +526,19 @@
 
     <!-- Core JS -->
     <script src="{{ asset('html/demo/app/assets/js/app.min.js') }}"></script>
+
+    <!-- DateTimePicker Bootstrap -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/js/bootstrap-datetimepicker.min.js"></script>
+
+    <script type="text/javascript">
+        $(function() {
+           $('#datetimepicker').datetimepicker();
+        });
+    </script>
+
 
 </body>
 
