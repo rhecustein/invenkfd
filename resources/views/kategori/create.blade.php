33c3de4feb57087b3733df8d88ace7da238a0d30
Faiz <faizadli9912@gmail.com>
2022-03-15 13:40:39 +0700

    fixed Laporan DataTable & update ui create kategori

--- resources/views/kategori/create.blade.php
+++ resources/views/kategori/create.blade.php
@@ -9,25 +9,40 @@
     <div class="alert alert-danger">
         {{ $error }}
     </div>
-@endforeach    
+@endforeach
 @endif
 
 @if (Session::has('success'))
 <div class="alert alert-success" role="alert">
-    {{ Session('success') }}    
-</div>    
+    {{ Session('success') }}
+</div>
 @endif
 
-<form action="{{ url('kategori.store') }}" method="POST">
-    @csrf
-    <div class="mb-3">
-        <label for="">Kategori</label>
-        <input type="text" class="form-control" name="name">
-    </div>
+<div class="layout">
+    <div class="horizontal-layout">
+        <div class="container">
+            <div class="main">
+                <div class="card">
+                    <div class="card-body">
+                        <form action="{{ url('kategori.store') }}" method="POST">
+                            @csrf
+                            <h2>Kategori <span class="badge bg-primary">Create</span></h2>
+                            <hr>
+                            <div class="mb-3">
+                                <label for="">Kategori</label>
+                                <input type="text" class="form-control" name="name">
+                            </div>
 
-    <div class="mb-3">
-        <button class="btn btn-primary btn-block">Simpan Kategori</button>
+                            <div class="mb-3">
+                                <button class="btn btn-success btn-block">Simpan Kategori</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</form>
-    
+</div>
+
+
 @endsection
--- resources/views/laporan/laporan.blade.php
+++ resources/views/laporan/laporan.blade.php
@@ -1,9 +1,10 @@
 @extends('layouts.kfd')
 
 @section('css')
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.5.6/css/buttons.dataTables.min.css">
 
+    {{-- <link rel="stylesheet" href="{{ ('html/demo/app/assets/css/export.css') }}"> --}}
 @endsection
 
 @section('content')
@@ -60,24 +61,6 @@
                         <div class="card-body">
                             <h2>Data Laporan</h2>
                             <hr>
-
-                            {{-- <div class="header-nav-item">
-                                <div class="dropdown nav-item-select ">
-                                    <div class="toggle-wrapper btn btn-info" id="nav-profile-dropdown" data-bs-toggle="dropdown">
-                                        <span>Export</span>
-                                        <i class="feather icon-chevron-down"></i>
-                                    </div>
-                                    <div class="dropdown-menu">
-                                        <a href="#" class="dropdown-item">
-                                            <div class="d-flex align-items-center">
-                                                <i class="font-size-lg me-2 feather icon-user"></i>
-                                                <span>Export</span>
-                                             </div>
-                                         </a>
-                                    </div>
-                                </div>
-                            </div> --}}
-
                             <div class="table-responsive">
                                 <table id="filterTable" class="table table-hover">
                                     <thead>
@@ -115,69 +98,91 @@
 @endsection
 
 @push('scripts')
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.6/js/dataTables.buttons.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.flash.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/1.5.6/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js" type="text/javascript"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js" type="text/javascript"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js" type="text/javascript"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js" type="text/javascript"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js" type="text/javascript"></script>
 
     <script>
 
-    const table = $('#filterTable').DataTable({
-        "pageLength": 10,
-        "lengthMenu": [[10, 25, 50, 100], [10, 25, 50, 100]],
-        "bLengthChange": true,
-        "bFilter": true,
-        "processing": true,
-        "bServerSide": true,
-        "order": [[ 1, "asc" ]],
-        "ajax": {
-            url: "{{url('laporan_filter')}}",
-            type: "POST",
-            data:function(d){
-                d._token = "{{csrf_token()}}",
-                d.kateg = $("#kateg").val(),
-                d.fromdate = $("#fromdate").val(),
-                d.todate = $("#todate").val()
-            }
-        },
-        dom: 'Bfrtip',
-        buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
-        columns:[
-            {
-                "render":function(data, type, row, meta){
-                    return row.nama_inventaris
+        const table = $('#filterTable').DataTable({
+            "pageLength": 10,
+            "lengthMenu": [[5, 10, 25, 50, 100], [5, 10, 25, 50, 100]],
+            "bLengthChange": true,
+            "bFilter": true,
+            "processing": true,
+            "bServerSide": true,
+            "order": [[ 0, "asc" ]],
+            "ajax": {
+                url: "{{url('laporan_filter')}}",
+                type: "POST",
+                data:function(d){
+                    d._token = "{{csrf_token()}}",
+                    d.kateg = $("#kateg").val(),
+                    d.fromdate = $("#fromdate").val(),
+                    d.todate = $("#todate").val()
                 }
             },
-            {
-                "render":function(data, type, row, meta){
-                    return row.kategori.name
+            columns:[
+                {
+                    "render":function(data, type, row, meta){
+                        return row.nama_inventaris
+                    }
+                },
+                {
+                    "render":function(data, type, row, meta){
+                        return row.kategori.name
+                    }
+                },
+                {
+                    "render":function(data, type, row, meta){
+                        return row.qty_inventaris
+                    }
+                },
+                {
+                    "render":function(data, type, row, meta){
+                        return row.keterangan_inventaris
+                    }
+                },
+                {
+                    "render":function(data, type, row, meta){
+                        return moment(row.created_at).format('DD-MM-yy')
+                    }
                 }
-            },
-            {
-                "render":function(data, type, row, meta){
-                    return row.qty_inventaris
-                }
-            },
-            {
-                "render":function(data, type, row, meta){
-                    return row.keterangan_inventaris
-                }
-            },
-            {
-                "render":function(data, type, row, meta){
-                    return moment(row.created_at).format('DD-MM-yy')
-                }
-            }
-        ]
-    });
+            ],
+            dom: "Blfrtip",
+                buttons: [
+                    {
+                        text: 'csv',
+                        extend: 'csvHtml5',
+                    },
+                    {
+                        text: 'excel',
+                        extend: 'excelHtml5',
+                    },
+                    {
+                        text: 'pdf',
+                        extend: 'pdfHtml5',
+                    },
+                    {
+                        text: 'print',
+                        extend: 'print',
+                    },
+                ],
+                columnDefs: [{
+                    orderable: false,
+                    targets: -1
+                }]
+        });
 
-    function filter(){
-        table.ajax.reload(null,false)
-    }
+        function filter(){
+            table.ajax.reload(null,false)
+        }
+
+
     </script>
 @endpush
